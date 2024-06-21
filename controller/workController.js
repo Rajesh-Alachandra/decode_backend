d9a65f093fed988d0bdfd4287c51d364753a3994
unknown <rajeshalachandra001@gmail.com>
2024-06-21 15:30:20 +0530

    sec commit

--- controller/workController.js
+++ controller/workController.js
@@ -7,7 +7,7 @@ exports.createWork = async (req, res) => {
         const work = new Work({
             title,
             description,
-            date,
+            date: date || null,  // Ensure date is null if not provided
             company,
             challenge,
             solutions,
@@ -53,7 +53,7 @@ exports.getWorkById = async (req, res) => {
 
 exports.updateWork = async (req, res) => {
     const updates = Object.keys(req.body);
-    const allowedUpdates = ['title', "description", 'image', 'date', 'company', 'challenge', 'solutions', 'briefs'];
+    const allowedUpdates = ['title', 'description', 'image', 'date', 'company', 'challenge', 'solutions', 'briefs'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
     if (!isValidOperation) {
@@ -66,22 +66,23 @@ exports.updateWork = async (req, res) => {
             return res.status(404).send();
         }
 
-        updates.forEach((update) => (work[update] = req.body[update]));
+        updates.forEach((update) => {
+            if (req.body[update] !== undefined) {
+                work[update] = req.body[update];
+            }
+        });
+
+        if (req.file) {
+            work.image = {
+                filename: req.file.filename,
+                path: req.file.path,
+                mimetype: req.file.mimetype
+            };
+        }
+
         await work.save();
         res.send(work);
     } catch (error) {
         res.status(400).send(error);
-    }
-};
-
-exports.deleteWork = async (req, res) => {
-    try {
-        const work = await Work.findByIdAndDelete(req.params.id);
-        if (!work) {
-            return res.status(404).send();
-        }
-        res.send(work);
-    } catch (error) {
-        res.status(500).send(error);
     }
 };
