60aebafdc3f8e833f8e2b9e9b3d1f98973852739
unknown <rajeshalachandra001@gmail.com>
2024-06-21 14:27:37 +0530

    sec commit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/Decode', {
+        await mongoose.connect('mongodb+srv://rajeshalachandra001:bIn6Ina1zSS6Sz5q@sampleproject.gxncp2o.mongodb.net/Decode', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
