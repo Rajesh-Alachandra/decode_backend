5664b3a9492cc4ff04a01bac9664f106353badfa
Rajesh Alachandra <108992336+Rajesh-Alachandra@users.noreply.github.com>
2024-06-23 16:13:36 +0530

    chamges

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path'); // Add this line to require the 'path' module
 const connectDB = require('./config/database');
 const workRoutes = require('./routes/workRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -14,6 +15,9 @@ connectDB();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+// Middleware to serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use('/api', workRoutes);
